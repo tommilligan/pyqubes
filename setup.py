63e46ada1a782b9a94383614293d09f169ee831a
Tom Milligan <tom@tommilligan.net>
2017-04-20 13:59:30 +0100

    Removed support for Python 2.6

--- setup.py
+++ setup.py
@@ -15,6 +15,12 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: Apache Software License',
     ],
     zip_safe=False,
