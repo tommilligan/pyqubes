8f9bccf2dc8b5f0eac75efd896bcba85eb3e6a6d
Tom Milligan <tom@tommilligan.net>
2017-05-01 02:38:35 +0100

    Version bump to v0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqubes',
-    version='0.0.1',
+    version='0.0.3',
     license='Apache License 2.0',
     url='https://github.com/tommilligan/pyqubes/',
     author='Tom Milligan',
